b776702c8dfafe6f329bccfe1dab62d2b94cc341
GuilhermeKageyama <guiproc@gmail.com>
2021-10-25 12:20:46 -0300

    initial commit

---
+++ mesat/src/App.js
@@ -0,0 +1,14 @@
+import ImgToRender from "../src/HomemFeliz";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ImgToRender></ImgToRender>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ mesat/src/HomemFeliz.js
@@ -0,0 +1,6 @@
+import homem from "../src/homem-feliz-dinheiro.jpg";
+const ImgToRender = () => {
+  return <img src={homem} alt="fsdfsf"></img>;
+};
+
+export default ImgToRender;
